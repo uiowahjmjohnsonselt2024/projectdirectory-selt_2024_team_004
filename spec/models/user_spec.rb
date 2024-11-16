9cfb37088dfcffaa509c4ef36db680158ec7a6aa
root <root@Marissa-Laptop>
2024-11-15 19:32:32 -0600

    Default construction invalid test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Tests for validations of instance variables
+  describe 'validations' do
+    it 'should be invalid on default construction' do
+      # On default construction, the user and its instance variables should be invalid
+      test_user = User.new
+      expect(test_user).not_to be_valid
+      expect(test_user).not_to be_valid
+      expect(test_user).not_to be_valid
+      expect(test_user).not_to be_valid
+    end
+  end
 end
