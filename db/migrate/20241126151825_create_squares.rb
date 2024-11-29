b4a65e0055e1727e7a881dd9c3369b91ee0e6549
root <root@Marissa-Laptop>
2024-11-29 16:53:17 -0600

    typo in squares migration

--- db/migrate/20241126151825_create_squares.rb
+++ db/migrate/20241126151825_create_squares.rb
@@ -1,6 +1,6 @@
 class CreateSquares < ActiveRecord::Migration[6.0]
   def change
-    create_table :worlds do |t|
+    create_table :squares do |t|
       t.string "square_id"
       t.string 'world_name'
       t.references :world, null: false, foreign_key: true
