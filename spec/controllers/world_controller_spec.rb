96e2db838dff513f3bad9b3133c32db5919a88f2
kvanorny <kvanorny@uiowa.edu>
2024-12-04 17:55:43 -0600

    Session Controller testing completed

--- spec/controllers/world_controller_spec.rb
+++ spec/controllers/world_controller_spec.rb
@@ -2,23 +2,13 @@ require 'spec_helper'
 require 'rails_helper'
 require 'action_controller_workaround'
 
-describe WorldsController do #type: controller do <- was getting an error on "controller"
-  describe 'Creating a new world' do
-    it "should yield a valid request when the create world button is selected" do
-      get new_world_path
-      response.should be_success
-    end
-    it 'should create a new world with my selected character options' do
-    end
-  end
-  describe "Destroying a World" do
-    it "should remove the given world from my lists of world upon deleting" do
-
-    end
-    describe "Accessing my world" do
-      it "should provide me with a list of all my worlds" do
-
-      end
-    end
-  end
+describe WorldsController, type: controller do
+  let(:user) {
+    User.create(
+      name: "Test User",
+      email: "test@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+  }
 end
