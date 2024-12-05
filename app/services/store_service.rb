6884866ae8b807237a3b6863cdca84798c1f58ae
kwixm <kwixm@uiowa.edu>
2024-12-05 14:27:32 -0600

    Created Teleport button; Consolidated css

--- app/services/store_service.rb
+++ app/services/store_service.rb
@@ -5,10 +5,10 @@ class StoreService
 
     # Base USD prices
     usd_prices = {
-      sea_shard: 0.75,
-      pile_of_shards: 7.00,
-      hat_of_shards: 30.00,
-      chest_of_shards: 50.00
+      sea_shard_1: 0.75,
+      pile_of_shards_10: 7.50,
+      hat_of_shards_50: 30.00,
+      chest_of_shards_100: 50.00
     }
 
     # Convert prices to the user's selected currency
