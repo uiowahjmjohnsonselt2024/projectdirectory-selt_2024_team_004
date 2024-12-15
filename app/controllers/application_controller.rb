df2f12d20a54219913bc0f7df978d759ea215c99
kwixm <kwixm@uiowa.edu>
2024-12-14 20:32:12 -0600

    cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   end
 
   private
-  
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
--- app/models/user_world.rb
+++ app/models/user_world.rb
@@ -5,7 +5,7 @@ class UserWorld < ActiveRecord::Base
   private
 
   def generate_unique_user_world_id
-      self.user_world_id = SecureRandom.hex(10) # Generates a 20-character hex string
+    self.user_world_id = SecureRandom.hex(10) # Generates a 20-character hex string
   end
 
   belongs_to :user
